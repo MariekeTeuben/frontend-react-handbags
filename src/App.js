7d477061e862fd4d5b5444b6f9781f4b169a671c
MariekeTeuben <marieke@marenmaa.nl>
2022-09-07 11:15:53 +0200

    Button component toegevoegd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Button from './components/Button';
 import './App.css';
 
 function App() {
-  return (
+    return (
+      <>
       <h1>Handbags & Purses</h1>
+
+      <nav>
+          <Button isDisabled={false} clickHandler={() => console.log("to the collection")} > to the collection </Button>
+
+          <Button isDisabled={false} clickHandler={() => console.log("shop all bags")} > shop all bags </Button>
+
+          <Button isDisabled={true} clickHandler={() => console.log("pre-orders")} > pre-orders </Button>
+
+      </nav>
+
+      </>
   );
 }
 
---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Button({children, isDisabled, clickHandler}) {
+    return (
+        <button
+            type="button"
+            disabled={isDisabled}
+            onClick={clickHandler}
+            >
+            {children}
+        </button>
+    )
+}
+
+export default Button;
